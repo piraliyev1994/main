bda344456588261b5d2cbdc2c052186f64cf20be
piraliyev1994 <newserver1994@gmail.com>
2018-10-21 20:01:59 +0400

    main qovlugu yuklendi

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+
+
+/**
+ * 
+ */
+class all 
+{
+	
+	function __construct()
+	{
+	
+	}
+
+	function topla($a,$b)
+	{
+        return $a+$b;
+	}
+
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include "functions.php";
+
+$all=new all();
+
+echo $all->topla(10,5);
+
+
+
+?>
